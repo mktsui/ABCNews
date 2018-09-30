791c57867ddba73c1b438db35e23d7e427bb7136
Ryan <mktsui@ymail.com>
2018-10-01 04:48:52 +0800

    Second commit - bug fix

--- MovLancer/app/src/main/java/exam/freelancer/mktsui/movlancer/utilities/Constants.java
+++ MovLancer/app/src/main/java/exam/freelancer/mktsui/movlancer/utilities/Constants.java
@@ -1,8 +1,10 @@
 package exam.freelancer.mktsui.movlancer.utilities;
 
+import exam.freelancer.mktsui.movlancer.BuildConfig;
+
 public class Constants {
     public static final String LIST_POPULATED_KEY = "list_populated_key";
-    public static final String TMDB_API_KEY = "d8fc9e60355e28b6c29a240060c46063";
+    public static final String TMDB_API_KEY = BuildConfig.TMDB_API_KEY;
     public static final String TMDB_LANG = "en-US";
     public static final String API_BASE_URL = "https://api.themoviedb.org/3/";
     public static final String API_FFED = "movie/popular";
